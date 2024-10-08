ca9b5c750601ae0706eb9fd77fe62da6990e7328
Claudemir Donato Serigheli Junior <cdsjuniormm@gmail.com>
2024-10-08 03:05:44 -0300

    feat: add rotas app

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,13 @@ Route::get('/', 'PrincipalController@index');
 Route::get('/sobre', 'SobreController@index');
 
 Route::get('/contato', 'ContatoController@index');
+
+Route::get('/login', function() { return ''; });
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() { return ''; });
+
+    Route::get('/fornecedores', function() { return ''; });
+    
+    Route::get('/produtos', function() { return ''; });    
+});
