704c17133be7bd3e9c2dc4b0839af8525346620b
Claudemir Donato Serigheli Junior <cdsjuniormm@gmail.com>
2024-10-10 13:48:35 -0300

    feat: form login

---
+++ resources/views/site/login/form.blade.php
@@ -0,0 +1,24 @@
+@extends('site.base.section')
+@section('titulo', 'Contato')
+@section('conteudo')  
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina">
+            <h1>Login</h1>
+        </div>
+
+        <div class="informacao-pagina">
+            <div class="contato-principal" style="width: 30%; margin: auto;">
+                <form action="{{ route('auth.authenticate') }}" method="POST">
+                    @csrf
+                    <input name="email" type="email" placeholder="Login" class="borda-preta" value="{{ old('email') }}">
+                    {{ $errors->has('email') ? $errors->first('email') : '' }}
+                    <br>
+                    <input name="senha" type="password" placeholder="Senha" class="borda-preta" value="{{ old('senha') }}">
+                    {{ $errors->has('senha') ? $errors->first('senha') : '' }}
+                    <br>
+                    <button type="submit" class="borda-preta">ENVIAR</button>
+                </form>
+            </div>
+        </div>  
+    </div>
+@endsection
