5da70a6551e08dc5370df86b0d4100209d87e7ab
Claudemir Donato Serigheli Junior <cdsjuniormm@gmail.com>
2024-10-09 22:44:09 -0300

    refactor: mensagens de erro

--- resources/views/site/base/components/form-contato.blade.php
+++ resources/views/site/base/components/form-contato.blade.php
@@ -2,10 +2,13 @@
 <form action="{{ route('site.contato') }}" method="POST">
     @csrf
     <input name="nome" type="text" placeholder="Nome" class="{{ $class }}" value="{{ old('nome') }}">
+    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
     <br>
     <input name="telefone" type="text" placeholder="Telefone" class="{{ $class }}" value="{{ old('telefone') }}">
+    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
     <br>
     <input name="email" type="text" placeholder="E-mail" class="{{ $class }}" value="{{ old('email') }}">
+    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
     <br>
     <select name="motivo_contato_id" class="{{ $class }}">
         <option value="">Qual o motivo do contato?</option>
@@ -13,16 +16,11 @@
             <option value="{{ $motivo->id }}" {{ old('motivo_contato_id') == $motivo->id ? 'selected' : '' }}>{{ $motivo->nome }}</option>
         @endforeach
     </select>
+    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
     <br>
     <textarea name="mensagem" class="{{ $class }}">{{ old('mensagem') ?? 'Preencha aqui a sua mensagem'}}</textarea>
+    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
     <br>
     <button type="submit" class="{{ $class }}">ENVIAR</button>
 </form>
 
-@if(count($errors))
-    <div style="position: absolute; top: 0px; left: 0px; background-color: red; width: 100%;">
-        <pre>
-            {{ print_r($errors) }}
-        </pre>
-    </div>
-@endif
