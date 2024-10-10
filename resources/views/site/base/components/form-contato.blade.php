61318f691383bf3e23740c84d40fb5d82fd95eaa
Claudemir Donato Serigheli Junior <cdsjuniormm@gmail.com>
2024-10-09 22:48:48 -0300

    fix: name dos campos nas mensagens de erro

--- resources/views/site/base/components/form-contato.blade.php
+++ resources/views/site/base/components/form-contato.blade.php
@@ -5,10 +5,10 @@
     {{ $errors->has('nome') ? $errors->first('nome') : '' }}
     <br>
     <input name="telefone" type="text" placeholder="Telefone" class="{{ $class }}" value="{{ old('telefone') }}">
-    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
+    {{ $errors->has('telefone') ? $errors->first('telefone') : '' }}
     <br>
     <input name="email" type="text" placeholder="E-mail" class="{{ $class }}" value="{{ old('email') }}">
-    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
+    {{ $errors->has('email') ? $errors->first('email') : '' }}
     <br>
     <select name="motivo_contato_id" class="{{ $class }}">
         <option value="">Qual o motivo do contato?</option>
@@ -16,10 +16,10 @@
             <option value="{{ $motivo->id }}" {{ old('motivo_contato_id') == $motivo->id ? 'selected' : '' }}>{{ $motivo->nome }}</option>
         @endforeach
     </select>
-    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
+    {{ $errors->has('motivo_contato_id') ? $errors->first('motivo_contato_id') : '' }}
     <br>
     <textarea name="mensagem" class="{{ $class }}">{{ old('mensagem') ?? 'Preencha aqui a sua mensagem'}}</textarea>
-    {{ $errors->has('nome') ? $errors->first('nome') : '' }}
+    {{ $errors->has('mensagem') ? $errors->first('mensagem') : '' }}
     <br>
     <button type="submit" class="{{ $class }}">ENVIAR</button>
 </form>
